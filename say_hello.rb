a666fb59f00b50a37d10e79216748ff377a3fcd3
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-05 14:20:08 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,15 @@
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
+=======
+def say_hello(name = "Ruby Programmer!")
+  puts "Hello #{name}!"
+end
+
+say_hello("Gabriela")
+  puts "Hello #{name}!"
+say_hello
+
+def greeting(name)
+>>>>>>> c8abda43b0616a50c7409a390d6daab1182dbc5e
   puts "Hello #{name}!"
 end
